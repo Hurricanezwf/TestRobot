ef4044c2e9f845c1dd100a21a99b68babd9bcc63
Hurricanezwf <1094646850@qq.com>
2016-03-04 18:08:58 +0800

    optimize project structure

--- Login/control/check_login.php
+++ Login/control/check_login.php
@@ -2,12 +2,17 @@
 require_once '../../Common/network/http.php';
 
 $guid = $_POST['guid'];
+if (empty($guid)) {
+    header("Location: http://123.56.133.116/TestRobot/Login/view/login.html");
+    return;
+}
 
 $json = array(
+    'cmd' => 'cl_login_request',
     'guid' => $guid,
-    'id' => '1992',
 );
 $post_data=json_encode($json);
 $res = PostMsg(1, $post_data);
+
 echo $res;
 ?>
