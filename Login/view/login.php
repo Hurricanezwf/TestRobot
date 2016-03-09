58ff3fb0e8dee8003c66760fdd394002c9c40119
Hurricanezwf <1094646850@qq.com>
2016-03-09 17:00:50 +0800

    优化登陆界面

--- Login/view/login.php
+++ Login/view/login.php
@@ -1,10 +1,10 @@
-<?php
+<!--?php
 if (!session_id()) {
     session_start();
 }
 
 $ssid = session_id();
-?>
+?-->
 <!DOCTYPE HTML>
 <html>
 <head>
@@ -14,16 +14,35 @@ $ssid = session_id();
     <meta http-equiv="Cache" content="no-cache">
 
     <title>login</title>
+    <link rel="stylesheet" type="text/css" href="css/login.css"></link>
     <script type="text/javascript" src="../../Common/js/jquery-2.2.1.min.js"></script>
     <script type="text/javascript" src="js/login.js"></script>
 </head>
 
 <body>
-    <p>登陆</p>
-    <form action="../control/check_login.php" method="POST">
-        ID:<input type="text" id="guid" name="guid" />
-        <?php echo "<input type='hidden' name='ssid' value='$ssid' />"; ?>
-        <input type="button" id="login" value="login" />
-    </form>
+    <div class="top_div"></div>
+    <div class="middle_div">
+        <div class="image">
+            <div class="tou"></div>
+            <div class="left_hand"></div>
+            <div class="right_hand"></div>
+        </div>
+        <form action="../control/check_login.php" method="POST">
+            <p class="account">
+                <span class="account_logo"></span>
+                <input class="ipt" type="text" id="guid" placeholder="请输入账号" name="guid" />
+            </p>
+            <p style="position: relative;">
+                <span class="passwd_logo"></span> 
+                <input class="ipt" id="passwd" type="password" placeholder="无需输入密码" name="passwd" />
+            </p>
+            <!--?php echo "<input type='hidden' name='ssid' value='$ssid' />"; ?-->
+            <input class="sbm" type="button" id="login" value="login" />
+        </form>
+    </div>
+    <div class="bottom_div"></div>
+    <div class="footer">
+        <p style="font-family:Comic Sans MS">BY HURRICANEZWF</p>
+    </div>
 </body>
 </html>
